43024f1167922ae45618ed21b3a4f88a68f36b3c
Stephen Vertucci <steve.vertucci@gmail.com>
2023-01-25 20:59:15 -0500

    added 3 functions, added basket to return true, logged addition to array, empties the basket

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,31 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+const maxItems = 5
+let basket = []
+console.log( basket );
+
+// adds item and returns true
+function addItem ( item ) {
+    basket.push( item );
+    return true;
+}
+
+addItem ( 'Chips' );
+addItem ('Bagel');
+console.log( basket )
+
+function listItems () {
+    for (let grocery of basket) {
+        console.log(grocery);
+    }
+}
+
+
+function empty () {
+    basket = [];
+}
+
+empty(basket);
+console.log(basket);
+
